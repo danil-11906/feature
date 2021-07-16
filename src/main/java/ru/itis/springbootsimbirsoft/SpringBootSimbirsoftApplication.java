3a429dd0ebbbe2d1c987c584eaabb6e5dd467d27
Danil Barkov <55778811+danil-11906@users.noreply.github.com>
2021-07-16 11:57:41 +0300

    16.07
    pom.xml

---
+++ src/main/java/ru/itis/springbootsimbirsoft/SpringBootSimbirsoftApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.springbootsimbirsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimbirsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimbirsoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/springbootsimbirsoft/SpringBootSimbirsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.springbootsimbirsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimbirsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
